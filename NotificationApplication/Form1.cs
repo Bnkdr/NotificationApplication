8e19029cb9ceaca3e7f254cb99ca5bdacc1c9f04
Salih Zorbozan <salihzorbozan133@gmail.com>
2023-05-04 21:03:42 +0300

    v1.1.2

--- NotificationApplication/Form1.cs
+++ NotificationApplication/Form1.cs
@@ -13,6 +13,7 @@ using FireSharp.Response;
 using FireSharp.Interfaces;
 using System.Diagnostics;
 using System.IO;
+using Newtonsoft.Json;
 
 namespace NotificationApplication
 {
@@ -33,8 +34,9 @@ namespace NotificationApplication
         }
 
 
+        Çağrılan çağrılan = new Çağrılan();
 
-
+        List<Çağrılan> çağrılanlar = new List<Çağrılan>();
         private void Form1_Load(object sender, EventArgs e)
         {
 
@@ -54,9 +56,59 @@ namespace NotificationApplication
 
             string girisno = sr.ReadLine();
 
-
             txt_girisSife.Text = girisno;
 
+                FirebaseResponse res = client.Get(@"CurrentCall");
+                Dictionary<string, Çağrılan> data = JsonConvert.DeserializeObject<Dictionary<string, Çağrılan>>(res.Body.ToString());
+                çağrılanlar = new List<Çağrılan>(data.Values);
+            
+
+            string çağrılanNo = null;
+            string isimSoyisim = null;
+            string çağıranİdareciGorev = null;
+            string çağıranİdareciİsim = null;
+            string çağıranİdareciSoyisim = null;
+            string acıklama = null;
+
+            foreach(Çağrılan çağrılanl in çağrılanlar)
+            {
+               if( çağrılanl.çağrılanidarecino==çağrılan.getÇağrılanNo())
+                {
+                    çağrılanNo = çağrılanl.çağrılanidarecino;
+                    isimSoyisim = çağrılanl.idareciisimsoyisim;
+                    çağıranİdareciGorev = çağrılanl.çağıranidarecigorev;
+                    çağıranİdareciİsim = çağrılanl.çağıranidareciisim;
+                    çağıranİdareciSoyisim = çağrılanl.çağıranidarecisoyisim;
+                    acıklama = çağrılanl.acıklama;
+                }
+
+            }
+
+            Çağrılan çağrılan2 = new Çağrılan(çağrılanNo,isimSoyisim, çağıranİdareciGorev, çağıranİdareciİsim, çağıranİdareciSoyisim, acıklama);
+
+            while(true)
+            {
+             
+                    if(çağrılanlar!=null)
+                {
+                    for(int i=1;i<=5;i++)
+                    {
+                        if(çağrılan2.çağrılanidarecino==i.ToString())
+                        {
+                            MessageBox.Show($"{çağrılan2.çağıranidareciisim+ çağrılan2.çağıranidarecisoyisim + "(" + çağrılan2.çağıranidarecigorev + ")"}:{çağrılan2.acıklama}");
+                            client.Delete("CurrentCall/" + çağrılan2.çağrılanidarecino);
+                        }
+                        if(Convert.ToInt32(çağrılan2.çağrılanidarecino)>5)
+                        {
+                            MessageBox.Show($"{çağrılan2.çağıranidareciisim + çağrılan2.çağıranidarecisoyisim + "(" + çağrılan2.çağıranidarecigorev + ")"}, {çağrılan2.idareciisimsoyisim} adlı kişiyi çağırıyor: {çağrılan2.acıklama}");
+                            client.Delete("CurrentCall/" + çağrılan2.çağrılanidarecino);
+                        }
+                    }
+
+                   
+                }
+                
+            }
 
         }
 
@@ -68,26 +120,134 @@ namespace NotificationApplication
             if (girisno=="1")
             {
                 MessageBox.Show("Yadin Çelik(Müdür) olarak giriş yaptınız.");
+                this.Hide();
+                çağrılan.setÇağrılanNo(girisno);
             }
             else if (girisno == "2")
             {
                 MessageBox.Show("Şenay Öztürk(Müdür Başyardımcısı) olarak giriş yaptınız.");
+                this.Hide();
+                çağrılan.setÇağrılanNo(girisno);
             }
             else if (girisno == "3")
             {
                 MessageBox.Show("Galip Yavuz(Müdür Yardımcısı) olarak giriş yaptınız.");
+                this.Hide();
+                çağrılan.setÇağrılanNo(girisno);
             }
             else if (girisno == "4")
             {
                 MessageBox.Show("İsa Tevfik Kurşun(Müdür Yardımcısı) olarak giriş yaptınız.");
+                this.Hide();
+                çağrılan.setÇağrılanNo(girisno);
             }
             else if (girisno == "5")
             {
                 MessageBox.Show("Fikri Etem(Rehber Öğretmeni) olarak giriş yaptınız.");
+                this.Hide();
+                çağrılan.setÇağrılanNo(girisno);
+            }
+            else if (girisno == "091")
+            {
+                MessageBox.Show("9/A olarak giriş yaptınız.");
+                this.Hide();
+                çağrılan.setÇağrılanNo(girisno);
+            }
+            else if (girisno == "092")
+            {
+                MessageBox.Show("9/B olarak giriş yaptınız.");
+                this.Hide();
+                çağrılan.setÇağrılanNo(girisno);
+            }
+            else if (girisno == "093")
+            {
+                MessageBox.Show("9/C olarak giriş yaptınız.");
+                this.Hide();
+                çağrılan.setÇağrılanNo(girisno);
+            }
+            else if (girisno == "094")
+            {
+                MessageBox.Show("9/D olarak giriş yaptınız.");
+                this.Hide();
+                çağrılan.setÇağrılanNo(girisno);
+            }
+            else if (girisno == "101")
+            {
+                MessageBox.Show("10/A olarak giriş yaptınız.");
+                this.Hide();
+                çağrılan.setÇağrılanNo(girisno);
+            }
+            else if (girisno == "102")
+            {
+                MessageBox.Show("10/B olarak giriş yaptınız.");
+                this.Hide();
+                çağrılan.setÇağrılanNo(girisno);
+            }
+            else if (girisno == "103")
+            {
+                MessageBox.Show("10/C olarak giriş yaptınız.");
+                this.Hide();
+                çağrılan.setÇağrılanNo(girisno);
+            }
+            else if (girisno == "104")
+            {
+                MessageBox.Show("10/D olarak giriş yaptınız.");
+                this.Hide();
+                çağrılan.setÇağrılanNo(girisno);
+            }
+            else if (girisno == "111")
+            {
+                MessageBox.Show("11/A olarak giriş yaptınız.");
+                this.Hide();
+                çağrılan.setÇağrılanNo(girisno);
+            }
+            else if (girisno == "112")
+            {
+                MessageBox.Show("11/B olarak giriş yaptınız.");
+                this.Hide();
+                çağrılan.setÇağrılanNo(girisno);
+            }
+            else if (girisno == "113")
+            {
+                MessageBox.Show("11/C olarak giriş yaptınız.");
+                this.Hide();
+                çağrılan.setÇağrılanNo(girisno);
+            }
+            else if (girisno == "114")
+            {
+                MessageBox.Show("11/D olarak giriş yaptınız.");
+                this.Hide();
+                çağrılan.setÇağrılanNo(girisno);
+            }
+            else if (girisno == "121")
+            {
+                MessageBox.Show("12/A olarak giriş yaptınız.");
+                this.Hide();
+                çağrılan.setÇağrılanNo(girisno);
+            }
+            else if (girisno == "122")
+            {
+                MessageBox.Show("12/B olarak giriş yaptınız.");
+                this.Hide();
+                çağrılan.setÇağrılanNo(girisno);
+            }
+            else if (girisno == "123")
+            {
+                MessageBox.Show("12/C olarak giriş yaptınız.");
+                this.Hide();
+                çağrılan.setÇağrılanNo(girisno);
+            }
+            else if (girisno == "124")
+            {
+                MessageBox.Show("12/D olarak giriş yaptınız.");
+                this.Hide();
+                çağrılan.setÇağrılanNo(girisno);
             }
 
 
-
         }
+
+
+
     }
 }
--- NotificationApplication/Çağrılan.cs
+++ NotificationApplication/Çağrılan.cs
@@ -8,5 +8,39 @@ namespace NotificationApplication
 {
     internal class Çağrılan
     {
+        public string çağrılanidarecino { get; set; }
+        public string idareciisimsoyisim { get; set; }
+        public string çağıranidareciisim { get; set; }
+        public string çağıranidarecisoyisim { get; set; }
+        public string çağıranidarecigorev { get; set; }
+        public string acıklama { get; set; }
+
+        public Çağrılan()
+        {
+            
+        }
+
+        public Çağrılan(string çağrılanNO, string isimSoyisim, string çağıranİdareciİsim, string çağıranİdareciSoyisim, string çağıranİdareciGorev, string acıklama)
+        {
+            this.çağrılanidarecino = çağrılanNO;
+            this.idareciisimsoyisim = isimSoyisim;
+            this.çağıranidareciisim = çağıranİdareciİsim;
+            this.çağıranidarecisoyisim = çağıranİdareciSoyisim;
+            this.çağıranidarecigorev = çağıranİdareciGorev;
+            this.acıklama = acıklama;
+        }
+
+        internal void setÇağrılanNo(string no)
+        {
+            this.çağrılanidarecino = no;
+        }
+
+        internal string getÇağrılanNo()
+        {
+            return this.çağrılanidarecino;
+        }
+
+        
+
     }
 }
