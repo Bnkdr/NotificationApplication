1fe68acf07ce11c3370d3dc42be3217374680cb5
Salih Zorbozan <salihzorbozan133@gmail.com>
2023-05-04 13:10:48 +0300

    v1.1.1

--- NotificationApplication/Form1.Designer.cs
+++ NotificationApplication/Form1.Designer.cs
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txt_girisSife = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt_girisSife
+            // 
+            this.txt_girisSife.Location = new System.Drawing.Point(89, 93);
+            this.txt_girisSife.Name = "txt_girisSife";
+            this.txt_girisSife.Size = new System.Drawing.Size(138, 20);
+            this.txt_girisSife.TabIndex = 0;
+            this.txt_girisSife.TextChanged += new System.EventHandler(this.txt_girisSife_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(321, 227);
+            this.Controls.Add(this.txt_girisSife);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txt_girisSife;
     }
 }
 
--- NotificationApplication/Form1.cs
+++ NotificationApplication/Form1.cs
@@ -7,14 +7,87 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FireSharp;
+using FireSharp.Config;
+using FireSharp.Response;
+using FireSharp.Interfaces;
+using System.Diagnostics;
+using System.IO;
 
 namespace NotificationApplication
 {
     public partial class Form1 : Form
     {
+
+        IFirebaseConfig ifc = new FirebaseConfig()
+        {
+            AuthSecret = "qlDUgLSDUYM1OqcOnlecbAEDhbFWJI8MCMUtZpYU",
+            BasePath = "https://kkfldatabase-default-rtdb.europe-west1.firebasedatabase.app/"
+        };
+
+        IFirebaseClient client;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            try
+            {
+                client = new FirebaseClient(ifc);
+            }
+            catch
+            {
+                MessageBox.Show("There was an error");
+            }
+
+            //  this.Hide();
+
+
+            StreamReader sr = new StreamReader("C:\\Users\\LENOVO\\OneDrive\\Masaüstü\\test.txt");
+
+            string girisno = sr.ReadLine();
+
+
+            txt_girisSife.Text = girisno;
+
+
+        }
+
+        private void txt_girisSife_TextChanged(object sender, EventArgs e)
+        {
+
+            string girisno = txt_girisSife.Text;
+
+            if (girisno=="1")
+            {
+                MessageBox.Show("Yadin Çelik(Müdür) olarak giriş yaptınız.");
+            }
+            else if (girisno == "2")
+            {
+                MessageBox.Show("Şenay Öztürk(Müdür Başyardımcısı) olarak giriş yaptınız.");
+            }
+            else if (girisno == "3")
+            {
+                MessageBox.Show("Galip Yavuz(Müdür Yardımcısı) olarak giriş yaptınız.");
+            }
+            else if (girisno == "4")
+            {
+                MessageBox.Show("İsa Tevfik Kurşun(Müdür Yardımcısı) olarak giriş yaptınız.");
+            }
+            else if (girisno == "5")
+            {
+                MessageBox.Show("Fikri Etem(Rehber Öğretmeni) olarak giriş yaptınız.");
+            }
+
+
+
         }
     }
 }
