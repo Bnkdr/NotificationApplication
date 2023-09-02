907c529d09b2f70e4d14cc5d57198ee950226c73
Salih Zorbozan <salihzorbozan133@gmail.com>
2023-09-02 16:55:54 +0300

    v2.1.1

--- NotificationApplication/Form1.cs
+++ NotificationApplication/Form1.cs
@@ -14,6 +14,7 @@ using FireSharp.Interfaces;
 using System.Diagnostics;
 using System.IO;
 using Newtonsoft.Json;
+using System.Linq.Expressions;
 
 namespace NotificationApplication
 {
@@ -36,9 +37,11 @@ namespace NotificationApplication
 
         Çağrılan çağrılan = new Çağrılan();
         Çağrılan çağrılan2;
-        Geridönüt geridönüt = new Geridönüt();
+        string girisno = "";
+        
 
-       // List<Çağrılan> çağrılanlar = new List<Çağrılan>();
+
+        // List<Çağrılan> çağrılanlar = new List<Çağrılan>();
         private void Form1_Load(object sender, EventArgs e)
         {
 
@@ -52,27 +55,30 @@ namespace NotificationApplication
             }
 
             //  this.Hide();
-
-
-            StreamReader sr = new StreamReader("C:\\Users\\Public\\test.txt");
-
-            string girisno = sr.ReadLine();
-
-            txt_girisSife.Text = girisno;
-
-           
-
-            LiveData();
-
-
-                   
-                
-                
             
+            try
+            {
+                StreamReader sr = new StreamReader("C:\\Users\\LENOVO\\source\\repos\\NotificationApplication1\\NotificationApplication\\bin\\Debug\\test.txt");
+
+                girisno = sr.ReadLine();
+
+                txt_girisSife.Text = girisno;
+            }
+            catch{
+                MessageBox.Show("Giriş Id dosyası bulunamadı", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            LiveData(girisno);
+
+
+            
+
+
+
 
         }
 
-        async void LiveData()
+        async void LiveData(string girisno)
     {
             while(true)
             {
@@ -83,38 +89,34 @@ namespace NotificationApplication
                     FirebaseResponse res = await client.GetAsync($"CurrentCall/{çağrılan.getÇağrılanNo()}/");
                     Dictionary<string, string> data = JsonConvert.DeserializeObject<Dictionary<string, string>>(res.Body.ToString());
 
+                    
 
                     if (data.ElementAt(2).Value == çağrılan.getÇağrılanNo())
                     {
-                        çağrılan2 = new Çağrılan(data.ElementAt(2).Value, data.ElementAt(1).Value, data.ElementAt(3).Value, data.ElementAt(4).Value, data.ElementAt(5).Value, data.ElementAt(0).Value);
+                        çağrılan2 = new Çağrılan(data.ElementAt(2).Value, data.ElementAt(1).Value, data.ElementAt(5).Value, data.ElementAt(4).Value, data.ElementAt(3).Value, data.ElementAt(0).Value);
 
                         if (data.ElementAt(2).Value != null)
                         {
-                            for (int i = 1; i <= 5; i++)
+                            for (int i = 1; i <= 6; i++)
                             {
                                 if (çağrılan2.çağrılanidarecino == i.ToString())
                                 {
-                                    MessageBox.Show($"{çağrılan2.çağıranidareciisim + çağrılan2.çağıranidarecisoyisim + "(" + çağrılan2.çağıranidarecigorev + ")"}: {çağrılan2.acıklama}","Çağrı");
-                                    MessageBox.Show("Geridönüt vermek istiyor musunuz?","Geridönüt", MessageBoxButtons.OKCancel);
-                                    bool kontrol = Convert.ToBoolean(MessageBoxButtons.OKCancel);
-                                    if(kontrol==true)
-                                    {
-                                        geridönüt.Show();
-                                    }
+                                    MessageBox.Show($"{çağrılan2.çağıranidarecisoyisim} {çağrılan2.çağıranidareciisim + "(" + çağrılan2.çağıranidarecigorev + ")"}: {çağrılan2.acıklama}","Çağrı");
                                     client.Delete("CurrentCall/" + çağrılan2.çağrılanidarecino);
                                     break;
                                 }
 
                             }
-                            if (Convert.ToInt32(çağrılan2.çağrılanidarecino) > 5)
+                            if (Convert.ToInt32(çağrılan2.çağrılanidarecino) > 6 &&çağrılan2.çağrılanidarecino==girisno)
                             {
-                                MessageBox.Show($"{çağrılan2.çağıranidareciisim + çağrılan2.çağıranidarecisoyisim + "(" + çağrılan2.çağıranidarecigorev + ")"}, {çağrılan2.idareciisimsoyisim} adlı kişiyi çağırıyor: {çağrılan2.acıklama}");
+                                MessageBox.Show($"{çağrılan2.çağıranidareciisim} { çağrılan2.çağıranidarecisoyisim + "(" + çağrılan2.çağıranidarecigorev + ")"}, {çağrılan2.idareciisimsoyisim} adlı kişiyi çağırıyor: {çağrılan2.acıklama}");
                                 client.Delete("CurrentCall/" + çağrılan2.çağrılanidarecino);
 
                             }
                         }
 
                     }
+                   
                 }
                 this.Hide();
             }
@@ -128,6 +130,95 @@ namespace NotificationApplication
 
     }
 
+
+        private string noDegistirme(string no)
+        {
+          
+            if (no == "091")
+            {
+                no = "9A";
+                return no;
+            }
+            else if (no == "092")
+            {
+                no = "9B";
+                return no;
+            }
+            else if (no == "093")
+            {
+                no = "9C";
+                return no;
+            }
+            else if (no == "094")
+            {
+                no = "9D";
+                return no;
+            }
+            else if (no == "101")
+            {
+                no = "10A";
+                return no;
+            }
+            else if (no == "102")
+            {
+                no = "10B";
+                return no;
+            }
+            else if (no == "103")
+            {
+                no = "10C";
+                return no;
+            }
+            else if (no == "104")
+            {
+                no = "10D";
+                return no;
+            }
+            else if (no == "111")
+            {
+                no = "11A";
+                return no;
+            }
+            else if (no == "112")
+            {
+                no = "11B";
+                return no;
+            }
+            else if (no == "113")
+            {
+                no = "11C";
+                return no;
+            }
+            else if (no == "114")
+            {
+                no = "11D";
+                return no;
+            }
+            else if (no == "121")
+            {
+                no = "12A";
+                return no;
+            }
+            else if (no == "122")
+            {
+                no = "12B";
+                return no;
+            }
+            else if (no == "123")
+            {
+                no = "12C";
+                return no;
+            }
+            else if (no == "124")
+            {
+                no = "12D";
+                return no;
+            }
+            else
+            {
+                return "No bulunamadı";
+            }
+        }
         private void txt_girisSife_TextChanged(object sender, EventArgs e)
         {
 
@@ -258,6 +349,17 @@ namespace NotificationApplication
                 MessageBox.Show("12/D olarak giriş yaptınız.");
                 this.Hide();
                 çağrılan.setÇağrılanNo(girisno);
+            }
+            else if (girisno == "6")
+            {
+                MessageBox.Show("Formatör olarak giriş yaptınız.");
+                this.Hide();
+                çağrılan.setÇağrılanNo(girisno);
+            }
+            else
+            {
+                MessageBox.Show("Giriş yaptığınız kişi bulunamadı. Çıkış yapılıyor.", "Hata", MessageBoxButtons.OK);
+                Application.Exit();
             }
 
 
